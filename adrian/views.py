a6efd5bc739ca774518b7115999d27e171841f28
adrnspnu <adrnspnu@gmail.com>
2023-10-07 19:41:51 +0800

    m

--- adrian/views.py
+++ adrian/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+  return render(request, "base.html")
+def zxczxc(request):
+  return render(request, "index.html")
+def qwesd(request):
+  return render(request, "123.html")
